4dcc895bc162a94bcdd13f5bd9f1e0d447cf46b8
Christiane Magalhaes <christianem.arcari@gmail.com>
2023-01-21 17:39:26 -0300

    icones adicionados no alert

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ function add() {
   const dayExists = nlwSetup.dayExists(today)
 
   if (dayExists) {
-    alert("Dia já incluso" ❌)
+    alert("Dia já incluso! 🔴")
     return
   }
 
-  alert("Adicionado com sucesso!" ✔)
+  alert("Adicionado com sucesso! ✅")
   nlwSetup.addDay(today)
 }
 
